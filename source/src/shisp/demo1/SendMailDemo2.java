5cb18db7b412760724153bb110ad7bd1eed3df27
shisp <shisp@2345.com>
2017-11-24 14:00:10 +0800

    163密码修改为授权码

--- source/src/shisp/demo1/SendMailDemo2.java
+++ source/src/shisp/demo1/SendMailDemo2.java
@@ -2,6 +2,7 @@ package shisp.demo1;
 
 import java.util.Properties;
 
+import javax.mail.Address;
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -20,6 +21,8 @@ import javax.mail.internet.MimeMessage;
 public class SendMailDemo2 {
 
     public static void main(String[] args) {
+//        test(); //测试：新注册账号发送信息过滤较严格，554错误：信息被拦截
+        
         // 1.创建一个程序与邮件服务器会话对象 Session
         Properties props = new Properties();
         props.setProperty("mail.transport.protocol", "SMTP");
@@ -31,7 +34,49 @@ public class SendMailDemo2 {
         // 验证账号及密码，密码需要是第三方授权码
         Authenticator auth = new Authenticator() {
             public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("shispo@163.com", "88889999.");
+                return new PasswordAuthentication("shispo@163.com", "shi88889999");
+            };
+        };
+        Session session = Session.getInstance(props, auth);
+
+        // 2.创建一个Message，它相当于是邮件内容
+        Message message = new MimeMessage(session);
+        try {
+            // 设置发送者
+            message.setFrom(new InternetAddress("shispo@163.com"));
+            // 设置发送方式与接收者
+            message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress("shispo@163.com"));
+            // 设置主题
+            message.setSubject("关于java的mail说明");
+            // 设置内容
+            message.setContent("关于java的mail说明", "text/html;charset=utf-8");
+
+            // 3.创建 Transport用于将邮件发送
+            Transport.send(message);
+            
+            System.out.println("发送完成");
+        } catch (AddressException e) {
+            e.printStackTrace();
+            System.out.println("发送失败");
+        } catch (MessagingException e) {
+            System.out.println("发送失败");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void test(){
+     // 1.创建一个程序与邮件服务器会话对象 Session
+        Properties props = new Properties();
+        props.setProperty("mail.transport.protocol", "SMTP");
+        props.setProperty("mail.smtp.host", "smtp.163.com");
+        props.setProperty("mail.smtp.port", "25");
+        // 指定验证为true
+        props.setProperty("mail.smtp.auth", "true");
+        props.setProperty("mail.smtp.timeout", "1000");
+        // 验证账号及密码，密码需要是第三方授权码
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("shispo@163.com", "shi88889999");
             };
         };
         Session session = Session.getInstance(props, auth);
@@ -44,19 +89,26 @@ public class SendMailDemo2 {
             // 设置发送方式与接收者
             message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress("soogbo@163.com"));
             // 设置主题
-            message.setSubject("邮件发送测试");
+            message.setSubject("关于发布《车辆管理制度》的通知");
             // 设置内容
-            message.setContent("ceshi test", "text/html;charset=utf-8");
-
+            message.setContent("通知：参阅稍后邮件,", "text/html;charset=utf-8");
+//            InternetAddress internetAddress = new InternetAddress("soogbo@163.com");
+//            Address[] addressArr= {new InternetAddress("soogbo@163.com")};
             // 3.创建 Transport用于将邮件发送
+//            Transport transport = session.getTransport();
             Transport.send(message);
+            
+//            transport.sendMessage(message, addressArr);
+            
+            System.out.println("发送完成");
         } catch (AddressException e) {
             e.printStackTrace();
+            System.out.println("发送失败");
         } catch (MessagingException e) {
+            System.out.println("发送失败");
             e.printStackTrace();
         }
-        System.out.println("发送完成");
-    }
+    };
     
 
 }
